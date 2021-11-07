0908790f6879c5e981a4880f341e13a4646943e4
hamedhemati <hemati.hmd@gmail.com>
2021-11-07 14:41:07 +0100

    update joint_traning script

--- scripts/train_joint.sh
+++ scripts/train_joint.sh
@@ -2,8 +2,3 @@ python main_joint.py --strategy "JointTraining" \
       --data_dir /netscratch/mschreyer/deepNadim/100_datasets/philadelphia/original_data/city_payments_fy2017_encoded_all_new.csv \
       --no_epochs 200 --params_path "./params/params_10exp_scenario3.yml" --wandb_proj "ContinualAnomaly_JointTraining" \
       --bottleneck "tanh"
-
-python main_joint.py --strategy "JointTraining" \
-      --data_dir /netscratch/mschreyer/deepNadim/100_datasets/philadelphia/original_data/city_payments_fy2017_encoded_all_new.csv \
-      --no_epochs 2000 --params_path "./params/params_10exp_scenario3.yml" --wandb_proj "ContinualAnomaly_JointTraining" \
-      --bottleneck "tanh"
