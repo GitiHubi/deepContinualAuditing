851de7722a066d0c19bd4ad75463d29f35bcc6a2
hamedhemati <hemati.hmd@gmail.com>
2021-11-07 14:20:27 +0100

    update params and scripts

--- scripts/train_scenario3.sh
+++ scripts/train_scenario3.sh
@@ -16,9 +16,9 @@ python main_continual.py --strategy "EWC" \
 python main_continual.py --strategy "LwF" \
       --data_dir /netscratch/mschreyer/deepNadim/100_datasets/philadelphia/original_data/city_payments_fy2017_encoded_all_new_anomalies.csv \
       --no_epochs 100 --params_path "./params/params_10exp_scenario3.yml" --wandb_proj "ContinualAnomaly_Scenario3" \
-      --bottleneck "tanh" --lwf_alpha 1.0 --lwf_temperature 1.0
+      --bottleneck "tanh" --lwf_alpha 0.1 --lwf_temperature 2.0
 
 python main_continual.py --strategy "SynapticIntelligence" \
       --data_dir /netscratch/mschreyer/deepNadim/100_datasets/philadelphia/original_data/city_payments_fy2017_encoded_all_new_anomalies.csv \
       --no_epochs 100 --params_path "./params/params_10exp_scenario3.yml" --wandb_proj "ContinualAnomaly_Scenario3" \
-      --bottleneck "tanh" --si_lambda 1.0 --si_eps 0.01
+      --bottleneck "tanh" --si_lambda 5.0 --si_eps 0.001
