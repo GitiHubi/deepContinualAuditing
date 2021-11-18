430133483e662d935d7d679e44565c1fb1923927
hamedhemati <hemati.hmd@gmail.com>
2021-11-18 14:44:17 +0100

    UPDATE params and scripts

--- scripts/continual/train_continual_linear_chicago.sh
+++ scripts/continual/train_continual_linear_chicago.sh
@@ -3,7 +3,7 @@ do
   echo "Running for seed $seed"
   python main_continual.py --strategy "Naive" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
-        --no_epochs 100 --params_path "./params/params_10exp_scenario3.yml" --wandb_proj "deepNadim" \
+        --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml" --wandb_proj "deepNadim" \
         --bottleneck "tanh" --seed $seed --training_regime 'continual'
 done
 
@@ -13,7 +13,7 @@ do
   echo "Running for seed $seed"
   python main_continual.py --strategy "Replay" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
-        --no_epochs 100 --params_path "./params/params_10exp_scenario3.yml"  --wandb_proj "deepNadim" \
+        --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml"  --wandb_proj "deepNadim" \
         --bottleneck "tanh" --replay_mem_size 500 --seed $seed --training_regime 'continual'
 done
 
@@ -23,6 +23,6 @@ do
   echo "Running for seed $seed"
   python main_continual.py --strategy "EWC" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
-        --no_epochs 100 --params_path "./params/params_10exp_scenario3.yml"  --wandb_proj "deepNadim" \
+        --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml"  --wandb_proj "deepNadim" \
         --bottleneck "tanh" --ewc_lambda 50.0 --seed $seed --training_regime 'continual'
 done
