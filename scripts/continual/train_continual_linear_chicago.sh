951a940dd567c505ed87bdee7aabea1d9bfa6d0f
hamedhemati <hemati.hmd@gmail.com>
2021-11-18 17:20:09 +0100

    UPDATE scripts

--- scripts/continual/train_continual_linear_chicago.sh
+++ scripts/continual/train_continual_linear_chicago.sh
@@ -1,7 +1,7 @@
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main_continual.py --strategy "Naive" --dataset "chicago" \
+  python main.py --strategy "Naive" --dataset "chicago" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
         --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml" --wandb_proj "deepNadim" \
         --bottleneck "tanh" --seed $seed --training_regime 'continual'
@@ -11,7 +11,7 @@ done
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main_continual.py --strategy "Replay" --dataset "chicago" \
+  python main.py --strategy "Replay" --dataset "chicago" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
         --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml"  --wandb_proj "deepNadim" \
         --bottleneck "tanh" --replay_mem_size 500 --seed $seed --training_regime 'continual'
@@ -21,7 +21,7 @@ done
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main_continual.py --strategy "EWC" --dataset "chicago" \
+  python main.py --strategy "EWC" --dataset "chicago" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
         --no_epochs 100 --benchmark_configs_path "./benchmark_configs/linear.yml"  --wandb_proj "deepNadim" \
         --bottleneck "tanh" --ewc_lambda 50.0 --seed $seed --training_regime 'continual'
--- scripts/fromscratch/train_fromscratch_exponential_chicago.sh
+++ scripts/fromscratch/train_fromscratch_exponential_chicago.sh
@@ -1,7 +1,7 @@
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main_continual.py --strategy "Naive" --dataset "chicago" \
+  python main.py --strategy "Naive" --dataset "chicago" \
         --data_dir /netscratch/mschreyer/deepNadim/100_datasets/chicago/original_data/city_payments_encoded_all_new_anomalies.csv \
         --no_epochs 100 --benchmark_configs_path "./benchmark_configs/exponential.yml" --wandb_proj "deepNadim" \
         --bottleneck "tanh" --seed $seed --training_regime 'fromscratch'
